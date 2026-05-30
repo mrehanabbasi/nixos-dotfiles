ddc64ec75704cfb2a9084d3648e9ba325a4a0c64
M. Rehan Abbasi <mrehanabbasi@proton.me>
2026-05-30 23:06:19 +0500

    chore(flake): bump nixpkgs and home-manager to 26.05

--- flake.nix
+++ flake.nix
@@ -2,12 +2,12 @@
   description = "NixOS Dendritic Configuration";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-25.11";
+    nixpkgs.url = "nixpkgs/nixos-26.05";
     nixpkgs-unstable.url = "nixpkgs/nixos-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.11";
+      url = "github:nix-community/home-manager/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
